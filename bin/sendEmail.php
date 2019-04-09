40eb3579f670a3d5a21a9938de36efefca7cebbb
James Herzog <ck2294yp@metrostate.edu>
2019-04-09 14:10:01 -0500

    Sarting to implement MailGun

--- bin/sendEmail.php
+++ bin/sendEmail.php
@@ -54,10 +54,19 @@ function getTemplate($filename, $memInfo ) {
 
 // Sends activation email.
 function sendActivationEmail($adminEmailAddress, $sendTo) {
-    $memInfo = getUserData($sendTo);
-    $messageBody = getTemplate('emailTemplates/accountInfo.php', $memInfo);
-
-    $subject = "Welcome to TCI!";
+    # Include the Autoloader (see "Libraries" for install instructions)
+    require 'vendor/autoload.php';
+    ##use Mailgun\Mailgun;
+    # Instantiate the client.
+    $mgClient = new Mailgun('YOUR_API_KEY');
+    $domain = "YOUR_DOMAIN_NAME";
+    # Make the call to the client.
+    $result = $mgClient->sendMessage($domain, array(
+        'from'	=> 'Excited User <mailgun@YOUR_DOMAIN_NAME>',
+        'to'	=> 'Baz <YOU@YOUR_DOMAIN_NAME>',
+        'subject' => 'Hello',
+        'text'	=> 'Testing some Mailgun awesomness!'
+    ));
 
     // Sends the email out to the customer.
     mail("$sendTo", "$subject", "$messageBody", "From:" . $adminEmailAddress);
