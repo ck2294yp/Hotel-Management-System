bf1783f51e288d06ba29dd15c8d7a9b3c72136bd
James Herzog <ck2294yp@metrostate.edu>
2019-04-25 02:17:16 -0500

    Worked on some dynamic formatting Index page (WIP)

--- index.php
+++ index.php
@@ -31,19 +31,19 @@ if (sizeof($_REQUEST) > 0) {
         $checkInDate = "";
         $checkOutDate = "";
 
-    // If There are no problems. Then set the session to hold the values. Send the user to the "Searching rooms" page. (which will redirect
-      // them to the sign in page if user is not already logged in).
+        // If There are no problems. Then set the session to hold the values. Send the user to the "Searching rooms" page. (which will redirect
+        // them to the sign in page if user is not already logged in).
     } else {
         $_SESSION['checkInDate'] = $checkInDate;
         $_SESSION['checkOutDate'] = $checkOutDate;
 
         // Convert Date string back into date variables.
-        $checkInDate  = DateTime::createFromFormat('Y-m-d', $checkInDate);
+        $checkInDate = DateTime::createFromFormat('Y-m-d', $checkInDate);
         $checkOutDate = DateTime::createFromFormat('Y-m-d', $checkOutDate);
 
 
         // Get the stay duration.
-        $_SESSION['stayDuration'] = (date_diff($checkInDate,$checkOutDate))->format("%a");
+        $_SESSION['stayDuration'] = (date_diff($checkInDate, $checkOutDate))->format("%a");
         header('Location: searchRooms.php');
         exit;
     }
@@ -70,94 +70,94 @@ if (sizeof($_REQUEST) > 0) {
 <!--Navigation Bar-->
 <?php include 'bin/nav.php'; ?>
 
-<div class="details"     >
-    <form action="index.php" method="post">
-        <table>
-            <tr>
-                <th>Check-in Date</th>
-                <th>Check-out Date</th>
-            </tr>
-            <tr>
-                <th>
-                    <input type="date"
-                           id="checkInDate"
-                           name="checkInDate"
-                           required
-                           min=<?php echo($minStartDate); ?>
-                           max=<?php echo($maxStartDate); ?>
-                    >
-                </th>
-                <th>
-                    <input type="date"
-                           id="checkOutDate"
-                           name="checkOutDate"
-                           required
-                           min=<?php echo($minStartDate); ?>
-                           max=<?php echo($maxEndDate); ?>
-                    >
-                </th>
-            </tr>
-        </table>
-        <br>
+<section class="sec1Member">
+    <div class="details" style="padding-right: 10%">
+        <form action="index.php" method="post">
+            <table>
+                <tr>
+                    <th>Check-in Date</th>
+                    <th>Check-out Date</th>
+                </tr>
+                <tr>
+                    <th>
+                        <input type= "date"
+                               id= "checkInDate"
+                               name= "checkInDate"
+                               required
+                               min= <?php echo($minStartDate); ?>
+                               max= <?php echo($maxStartDate); ?>
+                        >
+                    </th>
+                    <th>
+                        <input type= "date"
+                               id= "checkOutDate"
+                               name= "checkOutDate"
+                               required
+                               min= <?php echo($minStartDate); ?>
+                               max= <?php echo($maxEndDate); ?>
+                        >
+                    </th>
+                </tr>
+            </table>
+            <br>
 
-        <input type="submit" value="Search Room">
+            <input type="submit" value="Search Room">
 
-    </form>
-</div>
-
-<!--homepage start -->
-<div class="image">
-    <div style="padding-left:400px">
-
+        </form>
     </div>
-</div>
 
-<div class="features">
-    <div>
-        <h2 style="text-align:center;">At TCI we offer different types of rooms, amenities, and our own exclusive
-            rewards program.</h2>
+    <!--homepage start -->
+    <div class="image">
+        <div style="padding-left:400px">
+
+        </div>
+    </div>
+</section>
+
+
+<section class="sec2Member">
+    <div class="features" style="padding-top: 10%; padding-bottom: 10%">
+        <div>
+            <h2 style="text-align:center;">At TCI we offer different types of rooms, amenities, and our own exclusive rewards program.</h2>
+                <ul style="padding-left:10%; padding-right:10%;">
+                    <h2>Specialty Rooms</h2>
+                    <li>Pets</li>
+                    <li>Chef</li>
+                    <li>Gaming</li>
+                </ul>
+                <ul style="padding-left:10%; padding-right:10%;">
+                    <h2>Amenities</h2>
+                    <li>Valet</li>
+                    <li>Gym</li>
+                    <li>Bar</li>
+                    <li>Wifi</li>
+                    <li>Arcade</li>
+                    <li>Pool</li>
+                    <li>Pet Ground</li>
+                </ul>
+                <ul style="padding-left:10%; padding-right:10%;">
+                    <h2>Rewards</h2>
+                    <li>Earn rewards for each stay</li>
+                    <li>Save money on your next stay</li>
+                </ul>
+        </div>
+    </div>
+    <div class="sr">
         <ul>
-            <ul style="padding-right:300px;padding-top:1px;" class="feat">
-                <li style="list-style-type:none;"><h2>Specialty Rooms</h2></li>
-                <li>Pets</li>
-                <li>Chef</li>
-                <li>Gaming</li>
-            </ul>
-            <ul>
-                <li style="list-style-type:none;padding-right:280px;"><h2>Amenities</h2></li>
-                <li>Valet</li>
-                <li>Gym</li>
-                <li>Bar</li>
-                <li>Wifi</li>
-                <li>Arcade</li>
-                <li>Pool</li>
-                <li>Pet Ground</li>
-            </ul>
-            <ul style="float:right;">
-                <li style="list-style-type:none;"><h2>Rewards</h2></li>
-                <li>Earn rewards for each stay</li>
-                <li>Save money on your next stay</li>
-            </ul>
+            <li>
+                <img src="https://www.rd.com/wp-content/uploads/2016/01/06-13-things-your-hotel-desk-clerk-wont-tell-you-concierge.jpg"
+                     style="width:500px;height:400px;" alt="Rewards photo"</li>
+            <li>
+                <div style="width:600px;height:400px;float:right;text-align:left;padding-left: 80px;">
+                    <h2 id="rewards"">Rewards</h2>
+                    <p>Twin Cities Inn has a rewards program!</p>
+                    <p>Earn rewards each time you book with Twins Cities Inn!</p>
+                    <p>Earn one point each night you stay.</p>
+                    <p>Use those points to save money on your next stay.</p></div>
+            </li>
         </ul>
     </div>
-</div>
-
-</div>
-<div class="sr">
-    <ul>
-        <li>
-            <img src="https://www.rd.com/wp-content/uploads/2016/01/06-13-things-your-hotel-desk-clerk-wont-tell-you-concierge.jpg"
-                 style="width:500px;height:400px;" alt="Rewards photo"</li>
-        <li>
-            <div style="width:600px;height:400px;float:right;text-align:left;padding-left: 80px;">
-                <h2 id="rewards"">Rewards</h2>
-                <p>Twin Cities Inn has a rewards program!</p>
-                <p>Earn rewards each time you book with Twins Cities Inn!</p>
-                <p>Earn one point each night you stay.</p>
-                <p>Use those points to save money on your next stay.</p></div>
-        </li>
-    </ul>
-</div>
+</section>
 
 <div style="text-align: center">
     <p> Contact Twin Cities Inn | Phone Number: 651-222-2020 | Email: TCIsupport@TwinCitiesInn.com </p>
