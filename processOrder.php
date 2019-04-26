2712388448cb5b506729c25f1bf26b0eb8a9490a
James Herzog <ck2294yp@metrostate.edu>
2019-04-25 20:51:07 -0500

    About to setup on-order-email.

--- processOrder.php
+++ processOrder.php
@@ -4,11 +4,30 @@ session_start();
 require_once "settings/settings.php";
 require_once "bin/inputSanitization.php";
 
+// Sanitizes input from the POST request.
+$_REQUEST['cardNum'] = sanitizeNumString($_REQUEST['cardNum']);
+$_REQUEST['memID'] = sanitizeNumString($_REQUEST['memID']);
+$_REQUEST['roomTypeID'] = sanitizeNumString($_REQUEST['roomTypeID']);
+$_REQUEST['checkInDate'] = sanitizeDateString($_REQUEST['checkInDate']);
+$_REQUEST['checkOutDate'] = sanitizeDateString($_REQUEST['checkOutDate']);
 
-#TODO DEBUG:
-print_r($_REQUEST);
+// Checks if there are any false (invalid) data coming into the database.
+if (in_array(false, $_REQUEST)){
+    echo "<script> alert(\"Invalid data entered, please try again.\"); </script>";
+    header('Location: billingPage.php');
+    exit;
+}
+
+// Stops if no session exists.
+if (array_key_exists('loggedIn', $_SESSION) === false) {
+    echo "<script> alert(\"Your session has timed out, please sign in again.\"); </script>";
+    header('Location: signIn.php');
+    exit;
+}
 
 
+
+// Processes order (makes insert call to the database).
 try {
     $conn = new PDO("mysql:host=$dbAddress;dbname=$dbLocation", $dbUsername, $dbPassword);
     # Set the PDO error mode to exception
@@ -17,11 +36,11 @@ try {
     # Creates prepared SQL statement to enter the order information
     $orderStmt = $conn->prepare("INSERT INTO InvoiceReservation (cardNum, memID, roomTypeID, invoiceStartDate, invoiceEndDate) 
               VALUES (:cardNum, :memID, :roomTypeID, :invoiceStartDate, :invoiceEndDate)");
-    $orderStmt->bindParam(':cardNum', sanitizeNumString($_REQUEST['cardNum']));
-    $orderStmt->bindParam(':memID', sanitizeNumString($_REQUEST['memID']));
-    $orderStmt->bindParam(':roomTypeID', sanitizeNumString($_REQUEST['roomTypeID']));
-    $orderStmt->bindParam(':invoiceStartDate', sanitizeDateString($_REQUEST['checkInDate']));
-    $orderStmt->bindParam(':invoiceEndDate', sanitizeDateString($_REQUEST['checkOutDate']));
+    $orderStmt->bindParam(':cardNum', $_REQUEST['cardNum']);
+    $orderStmt->bindParam(':memID', $_REQUEST['memID']);
+    $orderStmt->bindParam(':roomTypeID', $_REQUEST['roomTypeID']);
+    $orderStmt->bindParam(':invoiceStartDate', $_REQUEST['checkInDate']);
+    $orderStmt->bindParam(':invoiceEndDate', $_REQUEST['checkOutDate']);
 
     # Commit the changes.
     $conn->beginTransaction();
@@ -32,7 +51,9 @@ try {
     $conn = null;
 
     echo "<script> alert(\"Your order has been processed successfully! Thank you for booking with TCI!\"); </script>";
-    echo"Order processed successfully! Thank you for booking with TCI!";
+    echo"Order processed successfully! Thank you for booking with TCI! <br>";
+    echo "You should be redirected to the member's page. If that doesn't work, please click <a href=\"membersPage.php\">here</a>.";
+    header("membersPage.php");
 
 
 } catch (PDOException $e) {
@@ -42,10 +63,8 @@ try {
 
     # Sends a JavaScript alert message back to the user notifying them that there was an error processing their request.
     echo "<script> alert(\"We are sorry. There was a problem processing your request. Please try again, if problem persists please call TCI at 651-000-0000.\"); </script>";
-    #TODO DEBUG:
-    echo($e->getMessage());
-    //header('Location: billingPage.php');
-    //exit;
+    header('Location: billingPage.php');
+    exit;
 }
 
 
